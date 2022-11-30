5de2871572dde225138a1ec109406435ed7e16d9
kunhi <kunhimohamed6@gmail.com>
2022-11-30 19:06:47 +0400

    feat: gym trainer and respective child tables

--- sweat_zone/sweat_zone/doctype/gym_trainer/gym_trainer.js
+++ sweat_zone/sweat_zone/doctype/gym_trainer/gym_trainer.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, kunhimohamed6@gmail.com and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Gym Subscription Plan', {
+frappe.ui.form.on('Gym Trainer', {
 	// refresh: function(frm) {
 
 	// }
