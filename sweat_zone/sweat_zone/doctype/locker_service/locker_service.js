fb2e5a1bbdec15c7e52468be42c04d36c41c6fbd
kunhi <kunhimohamed6@gmail.com>
2022-12-02 17:49:53 +0400

    feat: locker doctype"

---
+++ sweat_zone/sweat_zone/doctype/locker_service/locker_service.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, kunhimohamed6@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Locker Service', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ sweat_zone/sweat_zone/doctype/locker_service/test_locker_service.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, kunhimohamed6@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLockerService(FrappeTestCase):
+	pass
