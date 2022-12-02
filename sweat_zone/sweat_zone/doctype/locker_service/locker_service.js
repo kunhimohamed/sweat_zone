54ecc5221a6f54b9c3f37fce02ea1403d8b2eb80
kunhi <kunhimohamed6@gmail.com>
2022-12-02 17:50:59 +0400

    feat: locker service

--- sweat_zone/sweat_zone/doctype/locker_service/locker_service.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, kunhimohamed6@gmail.com and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Locker Service', {
-	// refresh: function(frm) {
-
-	// }
-});
--- sweat_zone/sweat_zone/doctype/locker_service/locker_service.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, kunhimohamed6@gmail.com and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.utils.nestedset import NestedSet
-
-class LockerService(NestedSet):
-	pass
