5f958bf1cb812c3bd0bdcfdb6d31fd616ac16a17
kunhi <kunhimohamed6@gmail.com>
2022-12-21 15:50:58 +0400

    fix: change variable name from test

--- sweat_zone/sweat_zone/page/customer_profile_pag/customer_profile_pag.js
+++ sweat_zone/sweat_zone/page/customer_profile_pag/customer_profile_pag.js
@@ -17,10 +17,10 @@ make: function() {
    frappe.call('sweat_zone.events.customer.fetch_customer_subscription_details').then(r => {
       if (r.message){
          console.log(r.message);
-         $(frappe.render_template("customer_profile_pag", {"test":r.message, "array_length":r.message.length})).appendTo(this.page.main);
+         $(frappe.render_template("customer_profile_pag", {"subscription_details":r.message, "array_length":r.message.length})).appendTo(this.page.main);
       }
       else{
-         $(frappe.render_template("customer_profile_pag", {"test":[], "array_length":r.message.length})).appendTo(this.page.main);
+         $(frappe.render_template("customer_profile_pag", {"subscription_details":[], "array_length":r.message.length})).appendTo(this.page.main);
       }
    });
    
